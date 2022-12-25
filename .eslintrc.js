5318c85e0265a6b585aeb39956340be62de42370
jjustin-chen <justin.chen@kdanmobile.com>
2022-12-25 14:13:49 +0800

    update: init global style constants

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'plugin:security/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2021,
+    ecmaFeatures: {
+      jsx: true,
+      arrowFunctions: true,
+    },
+    sourceType: 'module',
+  },
+  plugins: ['react', 'jest', 'prettier', 'security'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'prettier/prettier': ['error', { jsxSingleQuote: false }],
+    "react/react-in-jsx-scope": "off",
+  },
+};
---
+++ constants/styled.js
@@ -0,0 +1,89 @@
+import styled, { css } from 'styled-components';
+
+export const colors = {
+  primary: 'black',
+  secondary: '#FFF',
+};
+
+export const bgColors = {
+  primary: '#FFF',
+};
+
+const breakdown = (query) => (style) =>
+  css`
+    @media screen and (max-width: ${query + 'px'}) {
+      ${style};
+    } ;
+  `;
+
+export const breakpoints = {
+  xxxl: breakdown(1919),
+  xxl: breakdown(1439),
+  xl: breakdown(1279),
+  lg: breakdown(1023),
+  md: breakdown(767),
+  sm: breakdown(575),
+  xs: breakdown(479),
+};
+
+// container
+export const Container = styled.section`
+  max-width: 1170px;
+  padding: 0 12px;
+  margin: 0 auto;
+
+  ${breakpoints.xl(css`
+    max-width: 970px;
+  `)}
+  ${breakpoints.lg(css`
+    max-width: 750px;
+  `)};
+  ${breakpoints.md(css`
+    max-width: 535px;
+  `)}
+`;
+
+// font
+export const fontWeight = {
+  light: '200',
+  regular: '400',
+  bold: '700',
+};
+
+const typogrophy = css`
+  font-size: 1rem;
+  font-weight: ${fontWeight.regular};
+  color: ${({ color }) => color || `${colors.primary}`};
+`;
+
+export const P = styled.p`
+  ${typogrophy};
+`;
+
+export const H1 = styled.h1`
+  ${typogrophy};
+  font-size: 4rem;
+  font-weight: ${fontWeight.bold};
+`;
+
+export const H2 = styled.h2`
+  ${typogrophy};
+  font-size: 2.5rem;
+  font-weight: ${fontWeight.bold};
+`;
+
+export const H3 = styled.h3`
+  ${typogrophy};
+  font-size: 1.5rem;
+`;
+
+export const H4 = styled.h4`
+  ${typogrophy};
+  font-size: 1.25rem;
+`;
+
+// link
+export const Link = styled.a`
+  ${typogrophy};
+  text-decoration: ${({ isUnderline }) => (isUnderline ? 'unset' : 'none')};
+`;
